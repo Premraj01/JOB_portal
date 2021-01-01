48ddc6367008fbc4a769c45b00b44ed676ed3ca0
Premraj01 <premrajtripute777@gmail.com>
2021-01-02 01:42:22 +0530

    deployment commit

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,23 @@ dotenv.config()
 connectDB()
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 app.use('/api/candidates', candidateRoutes)
 
-app.get('/', (req, res) => {
-  res.send('API is Running...')
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is Running...')
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
